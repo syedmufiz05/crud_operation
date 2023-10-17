17022a9b92b185767e6dd5ee784016f9b4c49d66
Syed Mufiz <syedmufiz935@gmail.com>
2023-10-17 17:15:06 +0530

    Made some another changes...

--- src/main/java/com/example/demo/BlueArcusApplication.java
+++ src/main/java/com/example/demo/BlueArcusApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudOperationApplication {
+public class BlueArcusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudOperationApplication.class, args);
+		SpringApplication.run(BlueArcusApplication.class, args);
 	}
 
 }
