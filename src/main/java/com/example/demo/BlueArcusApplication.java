2a97268479eebf6f1e2c8a8ee971c6f84a8cca16
Syed Mufiz <syedmufiz935@gmail.com>
2023-11-06 11:36:16 +0530

    Deployed project on tomcat server

--- src/main/java/com/example/demo/BlueArcusApplication.java
+++ src/main/java/com/example/demo/BlueArcusApplication.java
@@ -2,9 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlueArcusApplication {
+public class BlueArcusApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BlueArcusApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BlueArcusApplication.class, args);
---
+++ src/main/java/com/example/demo/controller/ViewController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ViewController {
+    @RequestMapping(value = "/show", method = RequestMethod.GET)
+    public String showMsg() {
+        System.out.print("Hello server");
+        return "Hello server";
+    }
+}
