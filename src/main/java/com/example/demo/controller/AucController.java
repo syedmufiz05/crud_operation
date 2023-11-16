c2b2803a00eb8d9b905a22cd2807e2e3a7e49c92
Syed Mufiz <syedmufiz935@gmail.com>
2023-11-16 12:53:42 +0530

    Added cross origin...

--- src/main/java/com/example/demo/controller/AucController.java
+++ src/main/java/com/example/demo/controller/AucController.java
@@ -17,27 +17,32 @@ public class AucController {
     private AucServiceImpl aucService;
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @CrossOrigin("http://localhost:5173/")
     public ResponseEntity<AucDto> saveAucDetails(@RequestBody AucDto aucDto, HttpServletRequest httpServletRequest) throws JsonProcessingException {
         String authToken = httpServletRequest.getHeader("Authorization").replace("Bearer", "");
         return aucService.saveAucDetails(aucDto, authToken);
     }
 
     @RequestMapping(value = "/get/{imsi}", method = RequestMethod.GET)
+    @CrossOrigin("http://localhost:5173/")
     public ResponseEntity<AucDto> getAucDetails(@PathVariable("imsi") String imsi) {
         return aucService.getAucDetails(imsi);
     }
 
     @RequestMapping(value = "/get/all", method = RequestMethod.GET)
+    @CrossOrigin("http://localhost:5173/")
     public List<AucDto> getAllAucDetails() {
         return aucService.getAllAucDetails();
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.PUT)
+    @CrossOrigin("http://localhost:5173/")
     public ResponseEntity<AucDto> updateAucDetails(@RequestParam String imsi, @RequestBody AucDto aucDto) throws JsonProcessingException {
         return aucService.updateAucDetails(imsi, aucDto);
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    @CrossOrigin("http://localhost:5173/")
     public void deleteAucDetails(@RequestParam String imsi) {
         aucService.deleteAucDetails(imsi);
     }
--- src/main/java/com/example/demo/controller/VmsController.java
+++ src/main/java/com/example/demo/controller/VmsController.java
@@ -17,27 +17,32 @@ public class VmsController {
     private VmsService vmsService;
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @CrossOrigin("http://localhost:5173/")
     public ResponseEntity<VmsDto> saveVmsDetails(@RequestBody VmsDto vmsDto, HttpServletRequest httpServletRequest) throws JsonProcessingException {
         String authToken = httpServletRequest.getHeader("Authorization").replace("Bearer", "");
         return vmsService.saveVmsDetails(vmsDto, authToken);
     }
 
     @RequestMapping(value = "/get/{msisdn}", method = RequestMethod.GET)
+    @CrossOrigin("http://localhost:5173/")
     public ResponseEntity<VmsDto> getVmsDetails(@PathVariable("msisdn") String msisdn) {
         return vmsService.getVmsDetails(msisdn);
     }
 
     @RequestMapping(value = "/get/all", method = RequestMethod.GET)
+    @CrossOrigin("http://localhost:5173/")
     public List<VmsDto> getAllVmsDetails() {
         return vmsService.getAllVmsDetails();
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.PUT)
+    @CrossOrigin("http://localhost:5173/")
     public ResponseEntity<VmsDto> updateVmsDetails(@RequestParam String msisdn, @RequestBody VmsDto vmsDto) throws JsonProcessingException {
         return vmsService.updateVmsDetails(vmsDto, msisdn);
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    @CrossOrigin("http://localhost:5173/")
     public String deleteVmsDetails(@RequestParam String msisdn) {
         return vmsService.deleteVmsDetails(msisdn);
     }
