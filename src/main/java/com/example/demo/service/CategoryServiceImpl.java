77cf58dab01f8b3325679c5e3daf50b38f8fb970
Syed Mufiz <syedmufiz935@gmail.com>
2023-11-28 14:39:11 +0530

    Refactored vms module code 28/11/2023

--- src/main/java/com/example/demo/service/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/CategoryServiceImpl.java
@@ -56,7 +56,7 @@ public class CategoryServiceImpl implements CategoryService {
             Category categoryDb = category.get();
             CategoryDto categoryDto = new CategoryDto();
             categoryDto.setCategoryId(categoryDb.getId());
-            categoryDto.setName(categoryDb.getName().replace("\"", ""));
+            categoryDto.setName(fetchValueFromJsonData(categoryDb.getName()));
             return new ResponseEntity<>(categoryDto, HttpStatus.OK);
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new CustomMessage(HttpStatus.NOT_FOUND.value(), "Category Id does n't exist"));
